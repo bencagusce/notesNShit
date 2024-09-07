2b52928a61b281f05d1496aaccebe51024c06b06
Ruby <ruby.calmerman@gmail.com>
2024-09-07 23:08:26 +0200

    Added some comments

--- brög.cs
+++ brög.cs
@@ -2,8 +2,11 @@ namespace Battleship
 {
     class monoBehaviour
     {
+        // Public kan läsas och ändras utanför klassen
         public float xPos = 0.5f;
+        // Private kan bara läsas eller ändras i klassen
         private float yPos = 0.5f;
+        // public, private set kan läsas utanför klassen men bara ändras i klassen
         public float zPos { get; private set; }
         private int hp = 100;
 
@@ -33,12 +36,15 @@ namespace Battleship
         }
     }
 
+    // Om programmet använder klasser måste all annan kod ligga i program klassen
     class Program
     {
         
         
         monoBehaviour dave = new monoBehaviour();
 
+        // Om programmet använder klasser måste all logik (funktionsanrop och variabeländringar) ligga i funktioner. Main() körs när man kör programmet
+        // void är en return type, precis som int.TryParse() returnar en bool för om den lyckades returnar Main() inget (void)
         void Main()
         {
             dave.xPos = 5;
